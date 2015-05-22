f2eee511edd34ba1688eca609cf7df4467c1b72d
tristanj <tristanj@uoregon.edu>
2015-05-21 20:04:37 -0700

    cleaned up a bit

---
+++ Build/reader1F.cxx
@@ -0,0 +1,117 @@
+#include <vtkPolyData.h>
+#include <vtkPointData.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPoints.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkFloatArray.h>
+#include <vtkDoubleArray.h>
+#include <vtkCellArray.h>
+
+#include <vtkDataSetWriter.h>
+
+std::vector<Triangle>
+GetTriangles(void)
+{
+    vtkPolyDataReader *rdr = vtkPolyDataReader::New();
+    rdr->SetFileName("proj1e_geometry.vtk");
+    cerr << "Reading" << endl;
+    rdr->Update();
+    cerr << "Done reading" << endl;
+    if (rdr->GetOutput()->GetNumberOfCells() == 0)
+    {
+        cerr << "Unable to open file!!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    vtkPolyData *pd = rdr->GetOutput();
+/*
+vtkDataSetWriter *writer = vtkDataSetWriter::New();
+writer->SetInput(pd);
+writer->SetFileName("hrc.vtk");
+writer->Write();
+ */
+
+    int numTris = pd->GetNumberOfCells();
+    vtkPoints *pts = pd->GetPoints();
+    vtkCellArray *cells = pd->GetPolys();
+    vtkDoubleArray *var = (vtkDoubleArray *) pd->GetPointData()->GetArray("hardyglobal");
+    double *color_ptr = var->GetPointer(0);
+    //vtkFloatArray *var = (vtkFloatArray *) pd->GetPointData()->GetArray("hardyglobal");
+    //float *color_ptr = var->GetPointer(0);
+    vtkFloatArray *n = (vtkFloatArray *) pd->GetPointData()->GetNormals();
+    float *normals = n->GetPointer(0);
+    std::vector<Triangle> tris(numTris);
+    vtkIdType npts;
+    vtkIdType *ptIds;
+    int idx;
+    for (idx = 0, cells->InitTraversal() ; cells->GetNextCell(npts, ptIds) ; idx++)
+    {
+        if (npts != 3)
+        {
+            cerr << "Non-triangles!! ???" << endl;
+            exit(EXIT_FAILURE);
+        }
+        double *pt = NULL;
+        pt = pts->GetPoint(ptIds[0]);
+        tris[idx].X[0] = pt[0];
+        tris[idx].Y[0] = pt[1];
+        tris[idx].Z[0] = pt[2];
+        tris[idx].normals[0][0] = normals[3*ptIds[0]+0];
+        tris[idx].normals[0][1] = normals[3*ptIds[0]+1];
+        tris[idx].normals[0][2] = normals[3*ptIds[0]+2];
+        pt = pts->GetPoint(ptIds[1]);
+        tris[idx].X[1] = pt[0];
+        tris[idx].Y[1] = pt[1];
+        tris[idx].Z[1] = pt[2];
+        tris[idx].normals[1][0] = normals[3*ptIds[1]+0];
+        tris[idx].normals[1][1] = normals[3*ptIds[1]+1];
+        tris[idx].normals[1][2] = normals[3*ptIds[1]+2];
+        pt = pts->GetPoint(ptIds[2]);
+        tris[idx].X[2] = pt[0];
+        tris[idx].Y[2] = pt[1];
+        tris[idx].Z[2] = pt[2];
+        tris[idx].normals[2][0] = normals[3*ptIds[2]+0];
+        tris[idx].normals[2][1] = normals[3*ptIds[2]+1];
+        tris[idx].normals[2][2] = normals[3*ptIds[2]+2];
+
+        // 1->2 interpolate between light blue, dark blue
+        // 2->2.5 interpolate between dark blue, cyan
+        // 2.5->3 interpolate between cyan, green
+        // 3->3.5 interpolate between green, yellow
+        // 3.5->4 interpolate between yellow, orange
+        // 4->5 interpolate between orange, brick
+        // 5->6 interpolate between brick, salmon
+        double mins[7] = { 1, 2, 2.5, 3, 3.5, 4, 5 };
+        double maxs[7] = { 2, 2.5, 3, 3.5, 4, 5, 6 };
+        unsigned char RGB[8][3] = { { 71, 71, 219 },
+                                    { 0, 0, 91 },
+                                    { 0, 255, 255 },
+                                    { 0, 128, 0 },
+                                    { 255, 255, 0 },
+                                    { 255, 96, 0 },
+                                    { 107, 0, 0 },
+                                    { 224, 76, 76 }
+                                  };
+        for (int j = 0 ; j < 3 ; j++)
+        {
+            float val = color_ptr[ptIds[j]];
+            int r;
+            for (r = 0 ; r < 7 ; r++)
+            {
+                if (mins[r] <= val && val < maxs[r])
+                    break;
+            }
+            if (r == 7)
+            {
+                cerr << "Could not interpolate color for " << val << endl;
+                exit(EXIT_FAILURE);
+            }
+            double proportion = (val-mins[r]) / (maxs[r]-mins[r]);
+            tris[idx].colors[j][0] = (RGB[r][0]+proportion*(RGB[r+1][0]-RGB[r][0]))/255.0;
+            tris[idx].colors[j][1] = (RGB[r][1]+proportion*(RGB[r+1][1]-RGB[r][1]))/255.0;
+            tris[idx].colors[j][2] = (RGB[r][2]+proportion*(RGB[r+1][2]-RGB[r][2]))/255.0;
+        }
+    }
+
+    return tris;
+}
+
